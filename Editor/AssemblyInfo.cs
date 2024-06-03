9c21d29dc6cd92ccfbd5848763abfcc37de66c11
Dino Fejzagić <dino.f@live.de>
2024-06-03 19:04:59 +0200

    Fix assembly info [skip ci]

--- Editor/AssemblyInfo.cs
+++ Editor/AssemblyInfo.cs
@@ -1,7 +1,5 @@
-// Copyright (c) Reality Collective. All rights reserved.
+﻿// Copyright (c) Reality Collective. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
-
-#define REALITYCOLLECTIVE_BUILDTOOLS_EDITOR
 
 using System.Reflection;
 
